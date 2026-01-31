dead8a76c0e24dbb33f63eab66c13c553f6bd059
femboyloveer69 <femboyloveer69@gmail.com>
2026-01-31 15:36:29 +0100

    kezdolap teszteleshez+minta

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ def allowed_file(filename):
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    categories = get_all_categories()
+    return render_template("home.html", categories=categories)
 
 @app.route("/products")
 def products():
