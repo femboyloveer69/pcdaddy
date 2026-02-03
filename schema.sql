e771af00c238b131ffaf3e3e491689a3c629feaa
femboyloveer69 <femboyloveer69@gmail.com>
2026-02-03 08:48:41 +0100

    cart adatbazis frissites -quantity

--- schema.sql
+++ schema.sql
@@ -22,6 +22,5 @@ CREATE TABLE products (
 CREATE TABLE cart (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     product_id INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
     FOREIGN KEY (product_id) REFERENCES products(id)
 );
